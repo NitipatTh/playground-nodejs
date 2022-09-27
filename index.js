6d327d7fb064ec08295938737480e40aa7862a16
Nitipat Thanaborvornwiwat <nitipat@tokenx.finance>
2022-09-28 00:17:24 +0700

    edit index.js path

---
+++ index.js
@@ -0,0 +1,27 @@
+const express =  require ('express');
+
+const restaurantRouter = require('./src/routes/restaurants');
+
+const logger = require('./src/middleware/logger');
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Custom middleware
+app.use(logger);
+
+//Routes
+app.use('/apis/restaurants', (restaurantRouter));
+
+app.get('/', (req, res) => {
+    res.send('<br><br><center><h1>Node Express playground</h1></center><center><h4>Let\'s all make the day count</h4></center>')
+})
+
+// connection
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Listening to port 3000');
+})
