8ebe56c08543d8a8d48d62fe6aa4956d728f0891
Nitipat Thanaborvornwiwat <nitipat@tokenx.finance>
2022-10-28 11:59:54 +0700

    add avg block to query param

--- src/routes/utils.js
+++ src/routes/utils.js
@@ -6,9 +6,10 @@ const _ = require('lodash');
 router.get('/blockDate/:currentBlock', (req, res) => {
     const blockNumber = Number.parseInt(req.params.currentBlock, 10);
     const inputDate = dayjs(req.query.date);
+    const inputAvgBlock = req.query.avgBlock ? dayjs(req.query.avgBlock) : 3;
     const currentDate = dayjs();
     const diffHour = currentDate.diff(inputDate, 'hour')
-    const blockDate = blockNumber - (diffHour * 3600 / 3);
+    const blockDate = blockNumber - (diffHour * 3600 / inputAvgBlock);
     res.json({ data: { blockDate: blockDate, date: dayjs(inputDate).locale('th').format() } });
 })
 
